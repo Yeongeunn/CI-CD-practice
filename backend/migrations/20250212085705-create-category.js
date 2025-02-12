f45cae2da094fd53361e8bfda69e118f6ed368f4
Yeongeunn <nye0817@gmail.com>
2025-02-12 18:36:02 +0900

    feat: 마이그레이션 및 시더 적용, initDb.js 제거

---
+++ backend/migrations/20250212085705-create-category.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('categories', {
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      shelfLifeDays: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('categories');
+  }
+};
